dcc78e7204a51451b10d4ab7f4ac51bfa8479891
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2023-11-27 21:35:47 -0300

    add de logger.debug, logger.error

--- src/main/java/br/com/infnet/App.java
+++ src/main/java/br/com/infnet/App.java
@@ -23,27 +23,30 @@ public class App {
         System.out.println("Digite a opção");
         System.out.println("1 - C para k");
         System.out.println("2 - C para F");
-
-        switch (scanner.nextInt()) {
+        int option = scanner.nextInt();
+        switch (option) {
             case 1:
                 scanner.nextLine();
                 System.out.println("Digite a temperatura em C");
+                LOGGER.debug("Aguardando entrada da temperatura em C");
                 double celsius = scanner.nextDouble();
                 ConversorDeTemperaturas conversorDeTemperaturas = new ConversorDeTemperaturas();
                 LocalDateTime start = LocalDateTime.now();
                 LOGGER.info("Inicio do calculo de conversão");
+                LOGGER.debug("Iniciando conversão de Celsius para Kelvin");
                 double resultKelvin = conversorDeTemperaturas.celsiusParaKelvin(celsius);
                 LocalDateTime end = LocalDateTime.now();
                 long between = ChronoUnit.MILLIS.between(start, end);
                 LOGGER.info("Final do metodo de conversão com tempo: " + between + " MS.");
+                LOGGER.debug("Conversão concluída com tempo: {} MS.", between);
                 System.out.println("A temperatura em K é: " + "\u001B[32m" + resultKelvin + "\u001B[0m" + "k");
                 break;
 
             default:
+                LOGGER.error("Código inválido inserido pelo usuário: {}", option);
                 System.out.println("Invalid code");
                 break;
         }
-        LOGGER.info("Iniciando o Programa");
-
+        LOGGER.debug("Encerrando o programa");
     }
 }
